6db0aa30e1ddcc86480e0d0f29250778c704c1bf
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-07-31 09:41:43 -0700

    make all_contacts route

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,9 @@ class ContactsController < ApplicationController
     contact_one = Contact.first
     render json: {id: contact_one.id, first_name: contact_one.first_name, last_name: contact_one.last_name, email: contact_one.email, phone_number: contact_one.phone_number}
   end
+
+  def all
+    contacts = Contact.all
+    render json: {id: contacts}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "first_contact" => "contacts#contact_one"
+  get "all_contacts" => "contacts#all"
   # Defines the root path route ("/")
   # root "articles#index"
 end
