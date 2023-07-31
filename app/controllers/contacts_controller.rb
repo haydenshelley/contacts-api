331e3d9adeb6d4f59a55db6acf56bd1633a648d3
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-07-31 09:54:48 -0700

    fix contact_one route formatting

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,12 @@
 class ContactsController < ApplicationController
   def contact_one
     contact_one = Contact.first
-    render json: {id: contact_one.id, first_name: contact_one.first_name, last_name: contact_one.last_name, email: contact_one.email, phone_number: contact_one.phone_number}
+    render json: {
+      id: contact_one.id, 
+      first_name: contact_one.first_name, 
+      last_name: contact_one.last_name, 
+      email: contact_one.email, 
+      phone_number: contact_one.phone_number}
   end
 
   def all
