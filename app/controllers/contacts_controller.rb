82c2fb8ddb5357f60f0a4b236928d400294e5642
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-07-31 09:36:47 -0700

    make contacts controller, add contact_one route

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,6 @@
+class ContactsController < ApplicationController
+  def contact_one
+    contact_one = Contact.first
+    render json: {id: contact_one.id, first_name: contact_one.first_name, last_name: contact_one.last_name, email: contact_one.email, phone_number: contact_one.phone_number}
+  end
+end
