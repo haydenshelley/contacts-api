ac96973643efbdb14f613e111d5f366c2c1d921b
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-04 12:05:43 -0700

    add geocoder

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,12 +11,16 @@ class ContactsController < ApplicationController
   end
 
   def create
+    location = Geocoder.search(params[:address])
+
     @contact = Contact.create(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       phone_number: params[:phone_number],
-      address: params[:address]
+      address: params[:address],
+      lat: location.first.coordinates[0],
+      long: location.first.coordinates[1]
     )
     render :show
   end
--- app/views/contacts/_contact.json.jbuilder
+++ app/views/contacts/_contact.json.jbuilder
@@ -3,6 +3,8 @@ json.first_name contact.first_name
 json.last_name contact.last_name
 json.email contact.email
 json.address contact.address
+json.lat contact.lat
+json.long contact.long
 json.phone_number contact.phone_number
 json.created_at contact.created_at
 json.updated_at contact.updated_at
