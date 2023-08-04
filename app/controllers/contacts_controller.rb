2c2dd21aba59e29e79245f4430fc3d6f10c3cb8d
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-04 10:27:06 -0700

    create post

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,4 +10,15 @@ class ContactsController < ApplicationController
     render :show
   end
 
+  def create
+    @contact = Contact.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      address: params[:address]
+    )
+    render :show
+  end
+
 end
