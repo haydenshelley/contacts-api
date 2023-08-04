6bf286b0b434e5861fd14820e7da60d42fdffbc6
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-04 10:17:22 -0700

    add address column to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_185411) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_171602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_185411) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
